9e53c7e1f0c1654e4cc29f9d69d05c57eaafd536
John Freeman <john.freeman.opensource@gmail.com>
2016-09-06 07:15:05 +0100

    Pytest syntax improvements
    Paramaterized commands can use simple list.

--- tests/test_role.py
+++ tests/test_role.py
@@ -6,8 +6,8 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
 @pytest.mark.parametrize('command', [
-    ('java'),
-    ('javac')
+    'java',
+    'javac'
 ])
 def test_java_tools(Command, command):
     cmd = Command('. /etc/profile && ' + command + ' -version')
