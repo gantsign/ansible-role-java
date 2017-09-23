3ee1add8157c46dcc8d12e118c4ac926c5679252
John Freeman <john.freeman.opensource@gmail.com>
2017-09-23 19:19:40 +0100

    Added support for 9+181 release (#99)
    8u144 will remain the version installed by default.

---
+++ molecule/java9/tests/test_role.py
@@ -0,0 +1,43 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_java(Command):
+    cmd = Command('. /etc/profile && java -version')
+    assert cmd.rc == 0
+    assert 'java version "9"' in cmd.stderr
+
+
+def test_javac(Command):
+    cmd = Command('. /etc/profile && javac -version')
+    assert cmd.rc == 0
+    assert 'javac 9' in cmd.stdout
+
+
+def test_java_installed(File):
+
+    java_exe = File('/opt/java/jdk-9/bin/java')
+
+    assert java_exe.exists
+    assert java_exe.is_file
+    assert java_exe.user == 'root'
+    assert java_exe.group == 'root'
+    assert oct(java_exe.mode) == '0755'
+
+
+@pytest.mark.parametrize('fact_group_name', [
+    'java'
+])
+def test_facts_installed(File, fact_group_name):
+    fact_file = File('/etc/ansible/facts.d/' + fact_group_name + '.fact')
+
+    assert fact_file.exists
+    assert fact_file.is_file
+    assert fact_file.user == 'root'
+    assert fact_file.group == 'root'
+    assert oct(fact_file.mode) == '0644'
